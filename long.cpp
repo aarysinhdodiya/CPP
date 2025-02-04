c370f60a352b5bbc2f3b6b16252895e2d3e849a7
aarysinhdodiya <aarysinhdodiya05@gmail.com>
2025-02-04 12:22:37 +0530

    Add files via upload

---
+++ long.cpp
@@ -0,0 +1,9 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    cout << sizeof(int) << endl;            //4 byte
+    cout << sizeof(long int) << endl;       //8 byte
+    return 0;
+}
